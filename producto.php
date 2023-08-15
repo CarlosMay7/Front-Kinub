07dfcb5c902981be3cdb7fd9e8042ed061198905
Carlos May <carlosmay220@gmail.com>
2023-08-14 23:56:32 -0600

    Fin página producto

--- producto.php
+++ producto.php
@@ -21,22 +21,26 @@
     </div>
 </main>
 
-<nav class="navegacion">
-    <div class="navegacion__enlace">Descripción</div>
-    <div class="navegacion__enlace">Especificaciones técnicas</div>
-    <div class="navegacion__enlace">Área de descarga</div>
+<nav class="nav-producto">
+    <a href="#" class="nav-producto__enlace">Descripción</a>
+    <a href="#" class="nav-producto__enlace">Especificaciones técnicas</a>
+    <a href="#" class="nav-producto__enlace">Área de descarga</a>
 </nav>
 
 <section class="descripcion descripcion__contenedor">
     <h2 class="descripcion__heading">Descripción</h2>
-    <p class="descripcion__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magni pariatur vero doloremque, id ipsa praesentium a nesciunt! Ullam, porro laudantium. Voluptas fugit possimus vero explicabo rerum id omnis nesciunt.</p>
+    <div class="descripcion__texto-contenedor">
+        <p class="descripcion__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magni pariatur vero doloremque, id ipsa praesentium a nesciunt! Ullam, porro laudantium. Voluptas fugit possimus vero explicabo rerum id omnis nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum alias praesentium explicabo, nesciunt expedita ducimus dolor a, id maxime quasi, cum culpa tempora quibusdam et. Rerum itaque soluta quo suscipit.</p>
+    </div>
 </section>
 
 <section class="especificaciones especificaciones__contenedor">
     <h2 class="especificaciones__heading">Especificaciones Técnicas</h2>
-    <p class="especificaciones__texto"><span>Titulo:</span>Datos</p>
-    <p class="especificaciones__texto"><span>Titulo:</span>Datos</p>
-    <p class="especificaciones__texto"><span>Titulo:</span>Datos</p>
+    <div class="especificaciones__texto-contenedor">
+        <p class="especificaciones__texto"><span>Titulo:</span>Datos</p>
+        <p class="especificaciones__texto"><span>Titulo:</span>Datos</p>
+        <p class="especificaciones__texto"><span>Titulo:</span>Datos</p>
+    </div>
 </section>
 
 <?php 
@@ -46,18 +50,69 @@
 <section class="descarga descarga__contenedor">
     <h2 class="descarga__heading">Área de descarga</h2>
 
-
-    <div class="accordion accordion-flush" id="accordionFlushExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                Accordion Item #1
-            </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+    <div class="descarga__acordion-contenedor">
+        <div class="accordion accordion-flush" id="area-descarga">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#acordion-1" aria-expanded="false" aria-controls="acordion-1">
+                        Brochure
+                    </button>
+                </h2>
+                <div id="acordion-1" class="accordion-collapse collapse" data-bs-parent="#area-descarga">
+                    <ul class="descarga__lista">
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#acordion-2" aria-expanded="false" aria-controls="acordion-2">
+                        Datasheet
+                    </button>
+                </h2>
+                <div id="acordion-2" class="accordion-collapse collapse" data-bs-parent="#area-descarga">
+                    <ul class="descarga__lista">
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#acordion-3" aria-expanded="false" aria-controls="acordion-3">
+                        Manual de usuario
+                    </button>
+                </h2>
+                <div id="acordion-3" class="accordion-collapse collapse" data-bs-parent="#area-descarga">
+                    <ul class="descarga__lista">
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#acordion-4" aria-expanded="false" aria-controls="acordion-4">
+                        Certificados
+                    </button>
+                </h2>
+                <div id="acordion-4" class="accordion-collapse collapse" data-bs-parent="#area-descarga">
+                    <ul class="descarga__lista">
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                        <li><a href="/archivoTexto.txt" download>Descarga</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div> 
     </div>
 </section>
 
